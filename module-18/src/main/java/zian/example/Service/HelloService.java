4080e2ac209eb314567b695ed923321328fc5791
ljzhang <3131939824@qq.com>
2023-03-11 18:11:34 +0800

    ssm学完

---
+++ module-18/src/main/java/zian/example/Service/HelloService.java
@@ -0,0 +1,10 @@
+package zian.example.Service;
+/**
+ * @ClassName HelloService
+ * @Description
+ * @Author ljzhang
+ * @Date 2023/3/11 15:52
+ * @Version 1.0
+ */
+public interface HelloService {
+}
